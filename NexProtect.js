6788fd286be15a7ecc2a747cfc3341ae14e757ac
AhmetKesser <127592381+AhmetKesser@users.noreply.github.com>
2025-09-04 23:53:59 +0300

    Update NexProtect.js

--- NexProtect.js
+++ NexProtect.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 80;
 
 // Memory'de toplam istek sayısı
 let totalRequests = 0;
