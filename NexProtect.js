6476af73a0ee4cba9c4f689f4173de703fdf0421
AhmetKesser <127592381+AhmetKesser@users.noreply.github.com>
2025-09-05 00:12:33 +0300

    Update NexProtect.js

--- NexProtect.js
+++ NexProtect.js
@@ -6,6 +6,13 @@ const Port = 3000;
 
 App.use(Express.static(Path.join(__dirname, "NexProtect")));
 
+App.use((Req, Res, Next) => {
+    if (Req.httpVersionMajor !== 2) {
+        return Res.redirect("https://google.com");
+    }
+    Next();
+});
+
 App.get("/", (Req, Res) => {
     Res.sendFile(Path.join(__dirname, "NexProtect", "index.html"));
 });
